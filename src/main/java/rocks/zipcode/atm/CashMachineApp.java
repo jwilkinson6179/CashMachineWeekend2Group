f16a2ad7c10c29777204a13cebd4510f26b49bbe
James Wilkinson <JamesW6179@gmail.com>
2020-02-23 00:50:03 -0500

    Changed color of Login button

--- src/main/java/rocks/zipcode/atm/CashMachineApp.java
+++ src/main/java/rocks/zipcode/atm/CashMachineApp.java
@@ -97,16 +97,18 @@ public class CashMachineApp extends Application {
         GridPane.setConstraints(btnLogin, 1, 2);
 
         //btnLogin.setStyle("-fx-background-color: linear-gradient(#f0ff35, #a9ff00), radial-gradient(center 50% -40%, radius 200%, #b8ee36 45%, #80c800 50%); -fx-background-radius: 6, 5; -fx-background-insets: 0, 1; -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.4) , 5, 0.0 , 0 , 1 ); -fx-text-fill: #395306;");
-        btnLogin.setStyle("-fx-background-color: \n" +
-                "        #000000,\n" +
-                "        linear-gradient(#7ebcea, #2f4b8f),\n" +
-                "        linear-gradient(#426ab7, #263e75),\n" +
-                "        linear-gradient(#395cab, #223768);\n" +
-                "    -fx-background-insets: 0,1,2,3;\n" +
-                "    -fx-background-radius: 3,2,2,2;\n" +
-                "    -fx-padding: 12 30 12 30;\n" +
-                "    -fx-text-fill: white;\n" +
-                "    -fx-font-size: 12px;");
+        btnLogin.setStyle("    -fx-background-color: \n" +
+                "        linear-gradient(#ffd65b, #e68400),\n" +
+                "        linear-gradient(#ffef84, #f2ba44),\n" +
+                "        linear-gradient(#ffea6a, #efaa22),\n" +
+                "        linear-gradient(#ffe657 0%, #f8c202 50%, #eea10b 100%),\n" +
+                "        linear-gradient(from 0% 0% to 15% 50%, rgba(255,255,255,0.9), rgba(255,255,255,0));\n" +
+                "    -fx-background-radius: 30;\n" +
+                "    -fx-background-insets: 0,1,2,3,0;\n" +
+                "    -fx-text-fill: #654b00;\n" +
+                "    -fx-font-weight: bold;\n" +
+                "    -fx-font-size: 14px;\n" +
+                "    -fx-padding: 10 20 10 20;");
 
         btnLogin.setOnAction(e ->
         {
